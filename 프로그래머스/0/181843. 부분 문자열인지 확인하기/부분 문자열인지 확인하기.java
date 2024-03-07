29ffd17a60c5f3f52dd8c735637fbab0a51b4436
chldudms <147842532+chldudms@users.noreply.github.com>
2024-03-07 23:36:43 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my, String target) {
+        int answer = 0;
+      
+        
+        if(my.contains(target))
+            return 1;
+        else 
+            return 0;
+    }
+}
