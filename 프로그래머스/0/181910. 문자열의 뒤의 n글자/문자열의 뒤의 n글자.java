539ba22a16566c0dd7ace967b29a5b32e677ac36
chldudms <147842532+chldudms@users.noreply.github.com>
2024-03-14 11:17:48 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my, int n) {
+        String answer = " ";
+        
+         answer= my.substring(my.length()-n);
+        
+        return answer;
+    }
+}
