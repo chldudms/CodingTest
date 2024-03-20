df2cfc3c7a8f823dd745c1eb2cf1561f0dc8943a
chldudms <147842532+chldudms@users.noreply.github.com>
2024-03-20 22:58:00 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my, String is) {
+        int answer = 0;
+        
+//     for(int i=0; i<my.length(); i++){
+    
+//     }
+        if(my.startsWith(is))
+           return 1;
+       else
+           return 0;
+    }  
+    
+}
