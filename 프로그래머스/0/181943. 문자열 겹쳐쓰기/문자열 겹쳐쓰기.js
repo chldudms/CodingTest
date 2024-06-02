4ae7f36a50e4c9a8bf94fd8c8689693b17b2cf59
최여은 <147842532+chldudms@users.noreply.github.com>
2024-06-03 00:14:34 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,11 @@
+function solution(my, over, s) {
+    var answer = '';
+    var str = []
+    str = my.split("")
+    str.splice(s,over.length,over)
+     return str.join("")
+
+    
+    
+    
+}
