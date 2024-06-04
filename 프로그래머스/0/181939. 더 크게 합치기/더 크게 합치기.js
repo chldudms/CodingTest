0a8b7834e6c15ab2102bb38438c852421072f900
최여은 <147842532+chldudms@users.noreply.github.com>
2024-06-04 14:34:53 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,10 +1,7 @@
 function solution(a, b) {
     var answer = 0;
-    const a_str = a.toString()
-    const b_str = b.toString()
     
+    answer = a.concat(b)
     
-    answer = parseInt(a_str+b_str)>parseInt(b_str+a_str) ?a_str.concat("",b_str):b_str.concat("",a_str)
-    
-    return parseInt(answer);
+    return answer;
 }
