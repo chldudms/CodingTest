569ea131347937a158c7200c3ca40b7e68b726b6
최여은 <147842532+chldudms@users.noreply.github.com>
2024-08-11 23:19:29 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.51 ms, Memory: 34.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -0,0 +1,8 @@
+function solution(arr, queries) {
+    var answer = [];
+    
+   answer = queries.map((a,i)=>[arr[queries[i][0]],arr[queries[i][1]]]=[arr[queries[i][1]],arr[queries[i][0]]])
+    
+    
+    return arr;
+}
